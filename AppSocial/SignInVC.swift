2c15b922c4ed1c7dedd4683a614cd4bbf1b0173f
jsoliman2017 <solimanjuanito@yahoo.com>
2017-06-13 14:24:15 +0800

    adding firebase setup branch

---
+++ AppSocial/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppSocial
+//
+//  Created by Juanito Soliman on 13/06/2017.
+//  Copyright © 2017 Juanito Soliman. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
